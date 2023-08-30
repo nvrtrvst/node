b3be4d4c1d52b822bc626ad943cb1aec54978bd7
epo epo <devops@posindonesia.co.id>
2023-08-30 15:41:05 +0700

    add frontend

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -84,11 +84,63 @@ export const createProduct = async (req, res) => {
   }
 }
 
-export const updateProduct = (req, res) => {
+export const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id
+      }
+    });
+    if (!product) return res.status(404).json({ msg: "data tidak ditemukan" })
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.update({ name, price }, {
+        where: {
+          id: product.id
+        }
+      });
+    } else {
+      if (req.userId !== product.userId) return res.status(403).json({ msg: "Akses terlarang" });
+      await Product.update({ name, price }, {
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }]
+        },
+      });
+    }
+    res.status(200).json({ msg: "Produk Updated Succesfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 
 }
 
-export const deleteProduct = (req, res) => {
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id
+      }
+    });
+    if (!product) return res.status(404).json({ msg: "data tidak ditemukan" })
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.destroy({
+        where: {
+          id: product.id
+        }
+      });
+    } else {
+      if (req.userId !== product.userId) return res.status(403).json({ msg: "Akses terlarang" });
+      await Product.destroy({
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }]
+        },
+      });
+    }
+    res.status(200).json({ msg: "Deleted Succesfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 
 }
 
