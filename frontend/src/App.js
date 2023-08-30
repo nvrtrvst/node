15fa030dc9a2b3ffe409a53e30b62453f24326f3
epo epo <devops@posindonesia.co.id>
2023-08-30 15:41:25 +0700

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Dashboard = () => {
+  return   <Layout>  Lorem ipsum dolor sit </Layout>;
+};
+
+export default Dashboard
